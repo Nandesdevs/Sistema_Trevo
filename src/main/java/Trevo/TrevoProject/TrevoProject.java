7bc47067bab27773cd0b517d29e91c8cb89af2da
Aluno Alagoinhas <aluno.alagoinhas@senaibahia.com.br>
2026-09-19 16:00:51 -0300

    ALTEREI O PACKAGE

--- src/main/java/Trevo/TrevoProject/TrevoProject.java
+++ src/main/java/Trevo/TrevoProject/TrevoProject.java
@@ -1,13 +1,13 @@
-package Trevo.project;
+package Trevo.TrevoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class TrevoProject {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(TrevoProject.class, args);
 	}
 
 }
